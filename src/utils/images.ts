0a27e3f83a3df74cabff4a67124d8144f971a108
jkmprimedigitalprints <jkmprimedigitalprints@gmail.com>
2026-09-01 21:46:08 +0800

    Update JKM Digital Prints

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -34,10 +34,10 @@ export const ASSETS = {
   
   // Client Projects & Portfolio outputs
   clientProject1: '/Lyceum.png',
-  clientProject2: '/Accenture.png',
+  clientProject2: '/accenture.png',
   portfolio1: '/Lyceum.png',
   portfolio2: '/Calendar.png',
   portfolio3: '/Gwen.png',
   portfolio4: '/Doctors.jpg',
-  portfolio5: '/Accenture.png',
+  portfolio5: '/accenture.png',
 };
